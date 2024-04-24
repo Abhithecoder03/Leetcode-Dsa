1bb3622104ada8eff0a083cd4fcafeaf0296458f
Abhithecoder03 <107066993+Abhithecoder03@users.noreply.github.com>
2024-04-25 04:02:26 +0530

    Time: 758 ms (99.42%) | Memory: 24.7 MB (30.00%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedList
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+         
+        sorted_nums = SortedList()
+        count = 0
+        
+        for num in nums:
+            count += len(sorted_nums) - sorted_nums.bisect_right(2 * num)
+            sorted_nums.add(num)
+        
+        return count
+       
+
+            
