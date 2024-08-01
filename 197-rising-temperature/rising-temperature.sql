ad0404bb1bb3d786837a512e7da1d64f5c0b2c8c
Abhishek Chaurasia <107066993+Abhithecoder03@users.noreply.github.com>
2024-08-01 20:14:39 +0530

    Time: 468 ms (41.96%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -3,6 +3,6 @@ FROM (
     SELECT id, recordDate, temperature,
            LAG(temperature) OVER (ORDER BY recordDate) AS previous_temperature,
            LAG(recordDate) OVER (ORDER BY recordDate) AS previous_date
-    FROM Weather
-) subquery
+    FROM Weather 
+) as tab1
 WHERE temperature > previous_temperature AND DATEDIFF(recordDate,previous_date)=1;
